96168310379239e78e65d1aa98194020965ff2ac
Adw8 <adwait@deepstacksoft.com>
2024-06-13 12:10:24 +0530

    Export function to generate random keys

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import Schnorrkel from './schnorrkel'
 export { default as UnsafeSchnorrkel } from './unsafe-schnorrkel'
 
 export { Key, KeyPair, Signature, PublicNonces, Challenge, SignatureOutput, FinalPublicNonce } from './types'
-
+export { generateRandomKeys } from './core/index'
 export default Schnorrkel
