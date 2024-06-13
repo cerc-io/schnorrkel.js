3f8860fb894f76dcebf33f92dbb9763bc6bee05a
Adw8 <adwait@deepstacksoft.com>
2024-06-13 12:24:08 +0530

    Add line

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export { default as UnsafeSchnorrkel } from './unsafe-schnorrkel'
 
 export { Key, KeyPair, Signature, PublicNonces, Challenge, SignatureOutput, FinalPublicNonce } from './types'
 export { generateRandomKeys } from './core/index'
+
 export default Schnorrkel
